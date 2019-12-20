38bb2d0bfd77f727258c3c5fd2e31d57215a1c30
minhsl <khanh.nguyen@swisslife.ch>
2019-12-20 21:27:32 +0100

    styling project-titleo

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -36,10 +36,10 @@ class HomeSplash extends React.Component {
     );
 
     const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
+      <div className="projectTitle">
+        <h2>{siteConfig.title}</h2>
         <small>{siteConfig.tagline}</small>
-      </h2>
+      </div>
     );
 
     const PromoSection = props => (
